9bf8568f1bd55291962c1113f68641983faf2588
TheCrazy8 <bennettbashford@gmail.com>
2025-08-28 14:58:42 -0400

    Add VitePress configuration for Bigstone Wiki

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,30 @@
+export default {
+    title: "Bigstone Wiki",
+    description: "A wiki for Bigstone and our projects! Make sure your components align with the standards.",
+    themeConfig: {
+        siteTitle: "KCSgate Combat Data",
+        footer: {
+            message: "Released under the CC BY-NC-ND 4.0 License.",
+            copyright: "Copyright © 2025-present WD_Bypass",
+        },
+        nav: [
+        ],
+        sidebar: [
+        ],
+
+        socialLinks: [
+            { icon: "github", link: "https://github.com/WD-BYPASS" },
+        ],
+
+        lastUpdated: {
+              text: 'Updated at',
+              formatOptions: {
+                    dateStyle: 'full',
+                    timeStyle: 'medium'
+              }
+        },
+        search: {
+              provider: 'local'
+        },
+    },
+};
